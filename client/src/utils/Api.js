86a7b7fb82d1c8395fb3b337e262656ad09f811b
sudeepmi <sudeepm0599@gmail.com>
2024-05-04 14:13:56 +0545

    fix: register validation

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://anti-corruption-egov.onrender.com/api'
 export default  axios.create({
-    baseURL: baseURL,
+    baseURL: "https://anti-corruption-egov.onrender.com/api",
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Authorization': 'Bearer ' + localStorage.getItem('token')
