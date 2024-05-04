8e3ceb5d0cfd6c5aa1386c8ccb8403ea4fc59542
sudeepmi <sudeepm0599@gmail.com>
2024-05-04 14:15:28 +0545

    fix: navabr check

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -9,18 +9,18 @@ function Navbar() {
     window.location.reload()
   }
   return (
-    User() &&
+    User().user &&
     <nav className="navbar navbar-expand-lg navbar-dark">
-          <Link className="navbar_brand" to="/">Anti Corruption Portal</Link>
-          <Link className='navbar_links' to="/about">About Project</Link>
-          <Link className='navbar_links' to="/news">News</Link>
-          <Link className='navbar_links' to="/marketplace">Explore</Link>
-          <Link className='navbar_links' to="/dashboard">Dashboard</Link>
-          <Link>
-            <i className='fa fa-user'></i>
-            <span className='mx-1'>{User().user?.username}</span>
-          </Link>
-          <button className='navbar_links btn btn_small_brand' onClick={() => LogOut()}>Logout</button>
+      <Link className="navbar_brand" to="/">Anti Corruption Portal</Link>
+      <Link className='navbar_links' to="/about">About Project</Link>
+      <Link className='navbar_links' to="/news">News</Link>
+      <Link className='navbar_links' to="/marketplace">Explore</Link>
+      <Link className='navbar_links' to="/dashboard">Dashboard</Link>
+      <Link>
+        <i className='fa fa-user'></i>
+        <span className='mx-1'>{User().user?.username}</span>
+      </Link>
+      <button className='navbar_links btn btn_small_brand' onClick={() => LogOut()}>Logout</button>
     </nav>
   )
 }
